0d5ef067c00176b2d1a1d52472d8c31acefd5271
Jihad BENABRA <jihadbenabra@gmail.com>
2024-06-20 20:49:08 +0200

    Fix module import paths and update project structure

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
-git add .
 go mod tidy
 go build -o kclnr
+version=$(./kclnr version | awk '{print $3}')
+
+git add .
+git commit -m "Fix module import paths and update project structure"
+git push origin main
+
+git tag -a v$version -m "Release version v$version"
+git push origin v$version
