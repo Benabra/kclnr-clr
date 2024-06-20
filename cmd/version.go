1189ca418f23c34515c426d5e97400cbdcb2ec2f
Jihad BENABRA <jihadbenabra@gmail.com>
2024-06-20 20:54:23 +0200

    Fix module import paths and update project structure

--- cmd/version.go
+++ cmd/version.go
@@ -7,7 +7,7 @@ import (
 )
 
 // Set the version for the application
-const version = "0.16.3"
+const version = "0.16.4"
 
 // versionCmd represents the version command
 var versionCmd = &cobra.Command{
